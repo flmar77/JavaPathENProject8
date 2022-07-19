3e7039b7f6a06763b4e7b1f9b4ed8e3e9f73c59a
FMA <florian.marguerite@axa.fr>
2022-07-19 10:09:49 +0200

    model simplification

--- TourGuide/src/main/java/tourGuide/domain/model/UserReward.java
+++ TourGuide/src/main/java/tourGuide/domain/model/UserReward.java
@@ -2,29 +2,21 @@ package tourGuide.domain.model;
 
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class UserReward {
 
 	public final VisitedLocation visitedLocation;
 	public final Attraction attraction;
-	private int rewardPoints;
+	private final int rewardPoints;
+
 	public UserReward(VisitedLocation visitedLocation, Attraction attraction, int rewardPoints) {
 		this.visitedLocation = visitedLocation;
 		this.attraction = attraction;
 		this.rewardPoints = rewardPoints;
 	}
-	
-	public UserReward(VisitedLocation visitedLocation, Attraction attraction) {
-		this.visitedLocation = visitedLocation;
-		this.attraction = attraction;
-	}
 
-	public void setRewardPoints(int rewardPoints) {
-		this.rewardPoints = rewardPoints;
-	}
-	
-	public int getRewardPoints() {
-		return rewardPoints;
-	}
-	
 }
